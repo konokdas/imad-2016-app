02748d4ef21f7f269e667b1d16c60678794b494c
Konok Das <konoksmail@gmail.com>
2016-10-30 10:30:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article-one', function(req, res){
     res.send('Article One Requested');
 });
 
+app.get('/article-two', function(req, res){
+    res.send('Article Two Requested');
+});
+
+app.get('/article-three', function(req, res){
+    res.send('Article Three Requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
